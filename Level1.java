e543f04a008188536398b8391953c42568bde1e3
Bicky109 <chuckjak06@gmail.com>
2023-05-23 14:13:24 -0500

    stuff bugs: double entrance and wack level specific collison

--- Level1.java
+++ Level1.java
@@ -7,5 +7,25 @@ public class Level1 extends Level {
         setEnemy(0, new StillEnemy(250, 250, 40, 40,2));
         setStartingPosition(215,315);
     }
+
+    public void levelSpecificCollision()
+    {
+        if(getPlayer().getX() > 750 && getPlayer().getX() < 818 && getPlayer().getY() < 250)
+        {
+            if(getPlayer().getImageName().equals("images/playerleft.png") || getPlayer().getImageName().equals("images/playerleftnosword.png"))
+            {
+                getPlayer().setX(818);
+            }
+            else if(getPlayer().getImageName().equals("images/playerright.png") || getPlayer().getImageName().equals("images/playerright.png"))
+            {
+                getPlayer().setX(750);
+            }
+            //fix
+            else if(getPlayer().getImageName().equals("images/playerup.png") || getPlayer().getImageName().equals("images/playerupnosword.png"))
+            {
+                getPlayer().setY(250);
+            }
+        }
+    }
     
 }
